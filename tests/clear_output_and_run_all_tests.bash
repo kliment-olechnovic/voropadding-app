b28b3207da47863ee388cae87c3571b2d42e92af
klap <klap@klap-machine>
2025-08-06 17:55:02 +0200

    Added tests.

---
+++ tests/clear_output_and_run_all_tests.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SCRIPTDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPTDIR"
+
+rm -rf "./output"
+mkdir -p "./output"
+
+find ./input/ -type f \
+| grep -v '1cnw' \
+| while read -r INFILE
+do
+	BASENAME="$(basename ${INFILE} .pdb)"
+	echo "${INFILE} --output-table-file ./output/base_table_${BASENAME}.txt --output-graphics-file ./output/base_draw_${BASENAME}.py"
+done \
+| xargs -L 1 -P 4 ../voropadding --input
+
+
+../voropadding \
+  --input "./input/1cnw.pdb" \
+  --restriction-centers '[-aname ZN]' \
+  --restriction-radius 14 \
+  --output-table-file "./output/restricted_table_1cnw.txt" \
+  --output-graphics-file "./output/restricted_draw_1cnw.py"
+
+  
